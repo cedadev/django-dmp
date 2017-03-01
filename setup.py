87348e0bac8417cfea0570b4aa3f281ca6db1047
rsmith013 <richard.d.smith@stfc.ac.uk>
2017-03-01 15:04:48 +0000

    increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dmp',
-    version='1.6.1',
+    version='1.6.2',
     packages=['dmp'],
     include_package_data=True,
     license='my License',  # example license
