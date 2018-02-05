b4b72bb9a60eecaa338af85e5fdd2d34d069207c
rsmith013 <richard.d.smith@stfc.ac.uk>
2018-02-05 13:38:14 +0000

    Added helpscout URL field

--- dmp/admin.py
+++ dmp/admin.py
@@ -83,7 +83,7 @@ class ProjectAdmin(admin.ModelAdmin):
             ('startdate', 'enddate'), ('initial_contact','dmp_agreed'),
             ('sciSupContact', 'status'),
             ('primary_dataCentre','other_dataCentres'),
-            'dmp_URL','ODMP_URL','moles_URL')
+            'dmp_URL','ODMP_URL','moles_URL', 'helpscout_url')
         }),
         ('Advanced options', {
             'classes': ('collapse',),
