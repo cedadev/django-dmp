f65fd2eba7495e5a3659035f64aa89869333cd60
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-01-31 15:42:42 +0000

    making playbook work

--- dmphost/settings.py
+++ dmphost/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dmp',
+    'dmp.apps.DmpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
