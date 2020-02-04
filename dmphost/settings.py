4428c5560dfd610103a3d38ab477a4931aa42b57
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-02-04 14:55:55 +0000

    removed apps.py

--- dmphost/settings.py
+++ dmphost/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dmp.apps.DmpConfig',
+    'dmp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
