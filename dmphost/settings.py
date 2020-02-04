534caaa905f1d38cd0607d1daf64c7f90c1e9a09
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-02-04 12:27:10 +0000

    changed dirs

--- dmphost/settings.py
+++ dmphost/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dmphost.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', os.path.join(BASE_DIR, '/templates')],
+        'DIRS': [os.path.join(BASE_DIR, '/dmp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
